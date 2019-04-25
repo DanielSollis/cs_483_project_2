e134a96dea1f3f075b6639a1b561ab4c6a2ff42c
Michael Perna <michael.anthony.perna@gmail.com>
2019-04-25 15:21:57 -0700

    Edited p2-1.py and updated ipynb file

--- p2-1.py
+++ p2-1.py
@@ -18,6 +18,7 @@ X = pd.DataFrame(df.age)
 y = pd.DataFrame(df.wage)
 
 
+
 # Experiment 1
 def show_scatter(df):
     plt.scatter(df.age, df.wage, label='Data')
@@ -76,7 +77,7 @@ plt.legend()
 
 # Experiment 7 - added alpha values below .001 due to normalization
 ridge_cv = RidgeCV(alphas=[1e-07, 1e-06, 1e-05, 1e-04, 0.001, 0.002, 0.004, 0.01, 0.02, 0.04, 0.1, 0.2, 0.4, 1.0],
-                   store_cv_values=True, normalize=True).fit(X_train_4, y_train)
+                        normalize=True).fit(X_train_4, y_train)
 
 ridge_4_cv_pred = ridge_cv.predict(X_4)
 print('Fourth order RidgeCV coefficients', ridge_cv.coef_)
@@ -88,16 +89,14 @@ plt.figure(4)
 show_scatter(df)
 plt.plot(X, ridge_4_cv_pred, color='red', label='RidgeCV-Fourth')
 plt.legend()
-# plt.show()
+#plt.show()
 
 
 # Experiment 8
-X = df.drop(columns=['Unnamed: 0', 'year', 'race',
-                     'region', 'jobclass', 'health', 'health_ins', 'logwage', 'wage'])
+X = df.drop(columns=['Unnamed: 0', 'jobclass', 'logwage', 'wage'])
+
 y = df.logwage
 
-
-# one-hot encode categorical data
 def to_categorical(df):
     '''returns a new pd DataFrame with categorical data
     separated into columns of binary values'''
@@ -109,7 +108,7 @@ def to_categorical(df):
             del dfc[column]
     return dfc
 
-
+# one-hot encode categorical data
 X = to_categorical(X)
 X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                     test_size=0.2, shuffle=True)
@@ -119,46 +118,21 @@ coefs = pd.DataFrame(list(zip(X.columns, lm.coef_)),
                      columns=["Feature", "Coefficient"])
 print(coefs)
 
-poly = PolynomialFeatures(4)
-X_train_4 = poly.fit_transform(X_train)
-X_test_4 = poly.fit_transform(X_test)
-
-# Experiment 7 - added alpha values below .001 due to normalization
-ridge_cv = RidgeCV(alphas=[1e-07, 1e-06, 1e-05, 1e-04, 0.001, 0.002, 0.004, 0.01, 0.02, 0.04, 0.1, 0.2, 0.4, 1.0],
-                   store_cv_values=True, normalize=True).fit(X_train_4, y_train)
-
-ridge_4_cv_pred = ridge_cv.predict(X_test_4)
-print('Fourth order RidgeCV coefficients', ridge_cv.coef_)
-print('R^2 fourth order RidgeCV: ', ridge_cv.score(X_test_4, y_test))
-# Gives you the alpha value w/ the lowest MSE - 1e-06 is best for our normalized data
-print('Ridge_cv alpha: ', ridge_cv.alpha_)
-
 # Extra experiment to find the best order poly
-x = 50
-for i in range(1, 4):
+NUM_TRIALS = 50
+for degree in [1, 2]:
     r2 = 0
-    for x in range(x):
-        X = df.drop(columns=['Unnamed: 0', 'year',
-                             'region', 'logwage', 'wage'])
-        y = df.logwage
-
-        X = to_categorical(X)
-        X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                            test_size=0.2, shuffle=True)
-
-        lm = LinearRegression().fit(X_train, y_train)
-        coefs = pd.DataFrame(list(zip(X.columns, lm.coef_)),
-                             columns=["Feature", "Coefficient"])
-
-        poly = PolynomialFeatures(i)
-        X_train_4 = poly.fit_transform(X_train)
-        X_test_4 = poly.fit_transform(X_test)
+    for trial in range(NUM_TRIALS):
+        poly = PolynomialFeatures(degree)
+        X_train_poly = poly.fit_transform(X_train)
+        X_test_poly = poly.fit_transform(X_test)
 
         # Experiment 7 - added alpha values below .001 due to normalization
-        ridge_cv = RidgeCV(alphas=[1e-07, 1e-06, 1e-05, 1e-04, 0.001, 0.002, 0.004, 0.01, 0.02, 0.04, 0.1, 0.2, 0.4, 1.0],
-                           store_cv_values=True, normalize=True).fit(X_train_4, y_train)
+        ridge_cv = RidgeCV(alphas=[1e-07, 1e-06, 1e-05, 1e-04, 0.001, 0.002, 0.004, 0.01, 0.02, 0.04, 0.1, 0.2, 0.4, 1.0, 2.0, 3.0], 
+                            normalize=True).fit(X_train_poly, y_train)
 
-        ridge_4_cv_pred = ridge_cv.predict(X_test_4)
+        r2 += ridge_cv.score(X_test_poly, y_test)
 
-        r2 += ridge_cv.score(X_test_4, y_test)
-    print('R^2 ' + str(i) + ' order RidgeCV: ', str((r2/50)))
+    print('R^2 degree ' + str(degree) +  ' RidgeCV: ', str((r2/NUM_TRIALS)))
+    print('Alpha for degree ' + str(degree) + ' RidgeCV: ', ridge_cv.alpha_)
+
